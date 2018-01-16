538945feaa2a2f359341320ff2423d9b0265907b
minhhiends <minhhienpfiev@gmail.com>
2018-01-16 09:19:16 +0700

    Add file

--- Master_Project/main.py
+++ Master_Project/main.py
@@ -6,11 +6,11 @@ from kmeans_functions import*
 
 np.random.seed(11)
 #Main function
-K = 10
+K = 8
 (centers, labels, it) = kmeans(Y, K)
 P = kmeans_display(Y, labels[-1],centers[-1])
 
 #Save nodes and center
-f2 = open("Centers.txt", "w")
+f2 = open("Centers" + str(K) + ".txt", "w")
 f2.write("%s" %centers[-1])
 f2.close()
